296a0b79d7c5002fea76461c8a69aa77c662c48d
venkatkrishna <venkatkrishna@moe>
2025-02-11 18:30:09 +0100

    Reset to 0db1dfe365adb7a1973b6302359bc18c158bf720 and made changes for LLM Round 1

--- sdk/radicalbit_platform_sdk/models/dataset_data_quality.py
+++ sdk/radicalbit_platform_sdk/models/dataset_data_quality.py
@@ -8,7 +8,7 @@ class ClassMetrics(BaseModel):
     count: int
     percentage: Optional[float] = None
 
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
+    model_config = ConfigDict(populate_by_name=True)
 
 
 class MedianMetrics(BaseModel):
@@ -23,7 +23,7 @@ class MissingValue(BaseModel):
     count: int
     percentage: Optional[float] = None
 
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
+    model_config = ConfigDict(populate_by_name=True)
 
 
 class ClassMedianMetrics(BaseModel):
@@ -50,7 +50,7 @@ class NumericalFeatureMetrics(FeatureMetrics):
     max: Optional[float] = None
     median_metrics: MedianMetrics
     class_median_metrics: List[ClassMedianMetrics]
-    histogram: 'Histogram'
+    histogram: Histogram
 
     model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
 
