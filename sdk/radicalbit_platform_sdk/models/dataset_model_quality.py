22058b62c4201233c05f45eafb61a7f5694a24c4
venkatkrishna <venkatkrishna@moe>
2025-02-11 17:05:49 +0100

    Reset to 0db1dfe365adb7a1973b6302359bc18c158bf720 and made changes for LLM Round 1

--- sdk/radicalbit_platform_sdk/models/dataset_model_quality.py
+++ sdk/radicalbit_platform_sdk/models/dataset_model_quality.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
-from typing import Optional
+from typing import Optional, List
 
 
 class ModelQuality(BaseModel):
@@ -20,12 +20,13 @@ class BinaryClassificationModelQuality(ModelQuality):
     weighted_false_positive_rate: Optional[float] = None
     true_positive_rate: Optional[float] = None
     false_positive_rate: Optional[float] = None
-    true_positive_count: int
-    false_positive_count: int
-    true_negative_count: int
-    false_negative_count: int
+    true_positive_count: Optional[int] = None
+    false_positive_count: Optional[int] = None
+    true_negative_count: Optional[int] = None
+    false_negative_count: Optional[int] = None
     area_under_roc: Optional[float] = None
     area_under_pr: Optional[float] = None
+    histogram: Optional[List[float]] = None
 
     model_config = ConfigDict(
         populate_by_name=True, alias_generator=to_camel, protected_namespaces=()
