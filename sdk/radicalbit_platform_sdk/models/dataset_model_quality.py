89fc7bc466e49aea887cdc123536e8c589babd03
venkatkrishna <venkatkrishna@moe>
2025-02-11 15:33:06 +0100

    Reset to 0db1dfe365adb7a1973b6302359bc18c158bf720 and made changes for LLM Round 1

--- sdk/radicalbit_platform_sdk/models/dataset_model_quality.py
+++ sdk/radicalbit_platform_sdk/models/dataset_model_quality.py
@@ -3,38 +3,18 @@ from pydantic.alias_generators import to_camel
 from typing import Optional
 
 
-class ModelQuality(BaseModel):
+class DatasetStats(BaseModel):
     pass
 
 
-class BinaryClassificationModelQuality(ModelQuality):
-    f1: Optional[float] = None
-    accuracy: Optional[float] = None
-    precision: Optional[float] = None
-    recall: Optional[float] = None
-    f_measure: Optional[float] = None
-    weighted_precision: Optional[float] = None
-    weighted_recall: Optional[float] = None
-    weighted_f_measure: Optional[float] = None
-    weighted_true_positive_rate: Optional[float] = None
-    weighted_false_positive_rate: Optional[float] = None
-    true_positive_rate: Optional[float] = None
-    false_positive_rate: Optional[float] = None
-    true_positive_count: int
-    false_positive_count: int
-    true_negative_count: int
-    false_negative_count: int
+class BinaryClassificationStats(DatasetStats):
+    f1: float
+    accuracy: float
+    precision: float
+    recall: float
     area_under_roc: Optional[float] = None
     area_under_pr: Optional[float] = None
 
     model_config = ConfigDict(
         populate_by_name=True, alias_generator=to_camel, protected_namespaces=()
     )
-
-
-class MultiClassModelQuality(ModelQuality):
-    pass
-
-
-class RegressionModelQuality(ModelQuality):
-    pass
