06f5c87b92811ae5ec49f811cddcc635461eba74
venkatkrishna <venkatkrishna@moe>
2025-02-11 18:52:02 +0100

    Reset to 0db1dfe365adb7a1973b6302359bc18c158bf720 and made changes for LLM Round 1

--- sdk/radicalbit_platform_sdk/models/dataset_data_quality.py
+++ sdk/radicalbit_platform_sdk/models/dataset_data_quality.py
@@ -3,14 +3,6 @@ from pydantic.alias_generators import to_camel
 from typing import List, Optional, Union
 
 
-class ClassMetrics(BaseModel):
-    name: str
-    count: int
-    percentage: Optional[float] = None
-
-    model_config = ConfigDict(populate_by_name=True)
-
-
 class MedianMetrics(BaseModel):
     perc_25: Optional[float] = None
     median: Optional[float] = None
@@ -26,10 +18,26 @@ class MissingValue(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
 
+class ClassMetrics(BaseModel):
+    name: str
+    count: int
+    percentage: Optional[float] = None
+
+    model_config = ConfigDict(populate_by_name=True)
+
+
 class ClassMedianMetrics(BaseModel):
     name: str
     mean: Optional[float] = None
     median_metrics: MedianMetrics
+
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
+
+
+class Histogram(BaseModel):
+    buckets: List[float]
+    reference_values: List[int]
+    current_values: Optional[List[int]] = None
 
     model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
 
@@ -67,14 +75,6 @@ class CategoricalFeatureMetrics(FeatureMetrics):
     type: str = "categorical"
     category_frequency: List[CategoryFrequency]
     distinct_value: int
-
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
-
-
-class Histogram(BaseModel):
-    buckets: List[float]
-    reference_values: List[int]
-    current_values: Optional[List[int]] = None
 
     model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
 
